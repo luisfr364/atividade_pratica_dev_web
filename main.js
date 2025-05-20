dac68e0600b2d9ae52443a94a2022d0c62dae12b
luisfr364 <luisfr364@gmail.com>
2025-05-19 23:19:35 -0300

    comments added and few changes made

--- main.js
+++
@@ -1,9 +0,0 @@
-let contactLink = document.querySelectorAll(".contact-sec__ul__li");
-
-contactLink.forEach((element) => {
-  element.addEventListener("click", () => {
-    const link = element.getAttribute("link");
-    console.log(link);
-    const newWindow = window.open(link, "_blank");
-  });
-});
